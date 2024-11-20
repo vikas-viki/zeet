1bfc3106e3608e57e7f8e720f6a89b192aae99be
vikas-viki <viki57394@gmail.com>
2024-11-20 06:37:38 +0530

    created basic tilemap

--- src/components/Space.tsx
+++ src/components/Space.tsx
@@ -1,25 +1,77 @@
-import { Application, Loader } from 'pixi.js';
+import { Application, Assets } from 'pixi.js';
+import { CompositeTilemap } from "@pixi/tilemap";
 import { useEffect, useRef } from 'react';
+import { Map } from '../types/mapTypes';
 
 const Space = () => {
+    const canvasRef = useRef<HTMLDivElement>(null);
 
-    const app = new Application();
-    const canvasRef = useRef(null);
-    const loader = new Loader();
+    async function loadMap() {
+        Assets.addBundle('tilemap', {
+            map: '/src/map_assets/map.json',
+            garden: '/src/map_assets/garden.png',
+            pottedplants: '/src/map_assets/pottedplants.png',
+            office_pallete: '/src/map_assets/office_pallete.png'
+        });
 
-    app.init({
-        width: 600,
-        height: 600,
-        backgroundColor: 0x1099bb
-    }).then(() => {
-        if (canvasRef.current && canvasRef.current.hasChildNodes() == false)
-            canvasRef.current.appendChild(app.canvas);
-    });
+        const resources = await Assets.loadBundle('tilemap');
+        const mapData: Map = resources.map;
+        const tilemap = new CompositeTilemap();
+
+        mapData.layers.forEach((layer) => {
+            if (layer.type == "tilelayer") {
+                const tileSize = mapData.tilewidth;
+                layer.data.forEach((tileId, index) => {
+                    if (tileId == 0) return;
+
+                    const tileset = mapData.tilesets.find(tileset => (
+                        tileId >= tileset.firstgid
+                    ));
+
+                    if (tileset) {
+                        const texturename = tileset.name.toLowerCase();
+                        const tileX = (index % layer.width) * tileSize;
+                        const tileY = Math.floor(index / layer.width) * tileSize; // Fixed tileY calculation
+                        const localTileId = tileId - tileset.firstgid;
+
+                        const columns = tileset.imagewidth / tileSize;
+                        const textureX = (localTileId % columns) * tileSize;
+                        const textureY = Math.floor(localTileId / columns) * tileSize;
+
+                        const texture = resources[texturename];
+                        tilemap.tile(texture, tileX, tileY, {
+                            u: textureX,
+                            v: textureY,
+                            tileWidth: tileSize,
+                            tileHeight: tileSize
+                        });
+                    }
+                });
+            }
+        });
+        return tilemap;
+    }
+
+    useEffect(() => {
+        const app = new Application();
+
+        app.init({
+            width: 1200,
+            height: 900,
+            backgroundColor: 0x1099bb
+        }).then(async () => {
+            if (canvasRef.current && !canvasRef.current.hasChildNodes()) {
+                canvasRef.current.appendChild(app.canvas);
+                const tilemap = await loadMap();
+                console.log(tilemap);
+                app.stage.addChild(tilemap);
+            }
+        });
+    }, []);
 
     return (
-        <div ref={canvasRef}>
-        </div>
-    )
+        <div ref={canvasRef} />
+    );
 }
 
 export default Space;
