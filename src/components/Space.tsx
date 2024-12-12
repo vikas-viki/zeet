7471659398ce7b5c561d7051942e8d19572dd58f
vikas-viki <viki57394@gmail.com>
2024-12-12 20:58:28 +0530

    added collision system

--- src/components/Space.tsx
+++ src/components/Space.tsx
@@ -12,7 +12,7 @@ const Space = () => {
     const gameRef = useRef<Phaser.Game | null>(null);
     const player = useRef<Phaser.Physics.Arcade.Sprite | null>(null);
     var cursors: Phaser.Types.Input.Keyboard.CursorKeys | undefined;
-
+    var layers: Phaser.Tilemaps.TilemapLayer[] | null;
 
     useEffect(() => {
         if (!gameRef.current) {
@@ -29,7 +29,7 @@ const Space = () => {
                 physics: {
                     default: "arcade",
                     arcade: {
-                        debug: false
+                        debug: true
                     }
                 }
             }
@@ -48,8 +48,8 @@ const Space = () => {
             }
 
             function create(this: Phaser.Scene) {
-                const map = this.make.tilemap({ key: "map" });
-                // map.setCollision([775]);
+                const map = this.make.tilemap({ key: "map", tileWidth: 32, tileHeight: 32 });
+
                 const tileSets = [
                     map.addTilesetImage("floors"),
                     map.addTilesetImage("garden"),
@@ -57,15 +57,39 @@ const Space = () => {
                     map.addTilesetImage("pottedplants"),
                     map.addTilesetImage("walls")
                 ].filter(tileset => tileset != null);
+                const objectLayer = map.getObjectLayer("office_table");
 
+                const tableColliders: Phaser.GameObjects.Rectangle[] = [];
 
-                const layers = map.layers.map((layer, i) => map.createLayer(i, tileSets));
-                layers[1]?.setTint(0xff0000);
+                objectLayer?.objects.forEach(object => {
+                    if (object.x && object.y && object.width && object.height) {
+                        const collider = this.add.rectangle(
+                            object.x + object.width / 2,
+                            object.y + object.height / 2,
+                            object.width,
+                            object.height
+                        );
+
+                        this.physics.add.existing(collider, true);
+
+                        tableColliders.push(collider);
+                    }
+                });
+
+                layers = map.layers.map((layer, i) => map.createLayer(i, tileSets));
+
+                layers![1]?.setTint(0xff0000);
 
                 player.current = this.physics.add.sprite(400, 400, "player", 0);
 
-                // if (layers[1] != null)
-                //     this.physics.add.collider(player.current, layers[1]);
+                layers![0].setDepth(0);
+                player.current.setDepth(1);
+                layers![1].setDepth(1);
+                layers![2].setDepth(2);
+
+                tableColliders.forEach(collider => {
+                    this.physics.add.collider(player.current!, collider);
+                });
 
                 this.anims.create({
                     key: 'left',
@@ -95,6 +119,17 @@ const Space = () => {
                     repeat: -1
                 });
                 cursors = this.input.keyboard?.createCursorKeys();
+                if (layers![1])
+                    this.physics.add.overlap(player.current, layers![1], () => {
+                        const tile = map.getTileAtWorldXY(
+                            player.current!.x,
+                            player.current!.y
+                        );
+                        if (tile && (tile.index === 1006 || tile.index === 956)) {
+                            player.current?.setDepth(5);
+                            console.log("Specific tile collision detected!");
+                        }
+                    });
             }
 
             function update(this: Phaser.Scene) {
@@ -109,7 +144,7 @@ const Space = () => {
                         player.current.setVelocityX(160);
                         player.current.anims.play('right', true);
                     }
-                    
+
                     if (cursors.up.isDown) {
                         player.current.setVelocityY(-160);
                         player.current.anims.play('up', true);
@@ -118,11 +153,11 @@ const Space = () => {
                         player.current.setVelocityY(160);
                         player.current.anims.play('down', true);
                     }
-            
+
                     if (
-                        !cursors.left.isDown && 
-                        !cursors.right.isDown && 
-                        !cursors.up.isDown && 
+                        !cursors.left.isDown &&
+                        !cursors.right.isDown &&
+                        !cursors.up.isDown &&
                         !cursors.down.isDown
                     ) {
                         player.current.setVelocity(0);
