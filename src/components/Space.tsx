05b6e7281d871e84132f775c047d8edffb68ff7b
vikas-viki <viki57394@gmail.com>
2024-12-12 14:32:58 +0530

    added player movement

--- src/components/Space.tsx
+++ src/components/Space.tsx
@@ -1,170 +1,138 @@
 import Phaser from "phaser";
+import map from "../map_assets/map.json";
 import { useEffect, useRef } from "react";
-import sky from "../map_assets/phs/sky.png";
-import platform from '../map_assets/phs/platform.png';
-import star from '../map_assets/phs/star.png';
-import bomb from '../map_assets/phs/bomb.png';
-import dude from '../map_assets/phs/dude.png';
+import walls from "../map_assets/walls.png";
+import floors from "../map_assets/floors.png";
+import garden from "../map_assets/garden.png";
+import playerimg from "../map_assets/player.png";
+import pottedplants from "../map_assets/pottedplants.png";
+import offiece_pallete from "../map_assets/office_pallete.png";
 
 const Space = () => {
     const gameRef = useRef<Phaser.Game | null>(null);
-    var platforms: Phaser.Physics.Arcade.StaticGroup,
-        player: Phaser.Physics.Arcade.Sprite,
-        cursors: Phaser.Types.Input.Keyboard.CursorKeys,
-        stars: Phaser.Physics.Arcade.Group;
-    var score = 0;
-    var scoreText: Phaser.GameObjects.Text, bombs: Phaser.Physics.Arcade.Group;
+    const player = useRef<Phaser.Physics.Arcade.Sprite | null>(null);
+    var cursors: Phaser.Types.Input.Keyboard.CursorKeys | undefined;
 
 
     useEffect(() => {
         if (!gameRef.current) {
-
-
-            var config: Phaser.Types.Core.GameConfig = {
+            const config: Phaser.Types.Core.GameConfig = {
                 type: Phaser.AUTO,
-                width: 800,
-                height: 600,
+                width: 1000,
+                height: 800,
+                pixelArt: true,
                 scene: {
                     preload,
                     create,
                     update
                 },
                 physics: {
-                    default: 'arcade',
+                    default: "arcade",
                     arcade: {
-                        gravity: {
-                            y: 300,
-                            x: 0
-                        },
                         debug: false
                     }
                 }
-            };
+            }
             gameRef.current = new Phaser.Game(config);
 
-            function preload() {
-                this.load.image("sky", sky);
-                this.load.image('ground', platform);
-                this.load.image('star', star);
-                this.load.image('bomb', bomb);
-                this.load.spritesheet('dude',
-                    dude,
-                    { frameWidth: 32, frameHeight: 48 }
+            function preload(this: Phaser.Scene) {
+                this.load.tilemapTiledJSON("map", map);
+                this.load.image("floors", floors);
+                this.load.image("garden", garden);
+                this.load.image("office_pallete", offiece_pallete);
+                this.load.spritesheet("player", playerimg,
+                    { frameWidth: 32, frameHeight: 40 }
                 );
+                this.load.image("pottedplants", pottedplants);
+                this.load.image("walls", walls);
             }
 
-            function create() {
-                this.add.image(400, 300, "sky"); // center
+            function create(this: Phaser.Scene) {
+                const map = this.make.tilemap({ key: "map" });
+                // map.setCollision([775]);
+                const tileSets = [
+                    map.addTilesetImage("floors"),
+                    map.addTilesetImage("garden"),
+                    map.addTilesetImage("office_pallete"),
+                    map.addTilesetImage("pottedplants"),
+                    map.addTilesetImage("walls")
+                ].filter(tileset => tileset != null);
 
-                platforms = this.physics.add.staticGroup();
-                // width center, hight center.
-                platforms.create(400, 568, 'ground').setScale(2).refreshBody(); // nofity changes on scaling 'refreshbody'
-                platforms.create(600, 400, 'ground');
-                platforms.create(50, 250, 'ground');
-                platforms.create(750, 220, 'ground');
 
-                player = this.physics.add.sprite(100, 450, 'dude');
+                const layers = map.layers.map((layer, i) => map.createLayer(i, tileSets));
+                layers[1]?.setTint(0xff0000);
 
-                this.physics.add.collider(player, platforms);
+                player.current = this.physics.add.sprite(400, 400, "player", 0);
 
-                player.setBounce(0.2);
-                player.setCollideWorldBounds(true);
+                // if (layers[1] != null)
+                //     this.physics.add.collider(player.current, layers[1]);
 
                 this.anims.create({
                     key: 'left',
-                    frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+                    frames: this.anims.generateFrameNumbers('player', { start: 3, end: 5 }),
                     frameRate: 10,
                     repeat: -1
                 });
 
                 this.anims.create({
-                    key: 'turn',
-                    frames: [{ key: 'dude', frame: 4 }],
-                    frameRate: 20
+                    key: 'down',
+                    frames: this.anims.generateFrameNumbers('player', { start: 0, end: 2 }),
+                    frameRate: 10,
+                    repeat: -1
+                });
+
+                this.anims.create({
+                    key: 'up',
+                    frames: this.anims.generateFrameNumbers('player', { start: 6, end: 8 }),
+                    frameRate: 10,
+                    repeat: -1
                 });
 
                 this.anims.create({
                     key: 'right',
-                    frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+                    frames: this.anims.generateFrameNumbers('player', { start: 9, end: 11 }),
                     frameRate: 10,
                     repeat: -1
                 });
-
-                cursors = this.input.keyboard.createCursorKeys();
-
-                stars = this.physics.add.group({
-                    key: 'star',
-                    repeat: 11,
-                    setXY: { x: 12, y: 0, stepX: 70 }
-                });
-                this.physics.add.collider(stars, platforms);
-                this.physics.add.overlap(player, stars, collectStar, null, this);
-                stars.children.iterate((child: any) => child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8)));
-                scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
-                bombs = this.physics.add.group();
-
-                this.physics.add.collider(bombs, platforms);
-
-                this.physics.add.collider(player, bombs, hitBomb, null, this);
-
+                cursors = this.input.keyboard?.createCursorKeys();
             }
 
-            function collectStar(player: any, star: any) {
-                star.disableBody(true, true);
+            function update(this: Phaser.Scene) {
+                if (cursors && player.current) {
+                    player.current.setVelocity(0);
 
-                score += 10;
-                scoreText.setText('Score: ' + score);
-
-                if (stars.countActive(true) === 0) {
-                    stars.children.iterate(function (child: any) {
-                        return child.enableBody(true, child.x, 0, true, true);
-                    });
-
-                    var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
-
-                    var bomb = bombs.create(x, 16, 'bomb');
-                    bomb.setBounce(1);
-                    bomb.setCollideWorldBounds(true);
-                    bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+                    if (cursors.left.isDown) {
+                        player.current.setVelocityX(-160);
+                        player.current.anims.play('left', true);
+                    }
+                    else if (cursors.right.isDown) {
+                        player.current.setVelocityX(160);
+                        player.current.anims.play('right', true);
+                    }
+                    
+                    if (cursors.up.isDown) {
+                        player.current.setVelocityY(-160);
+                        player.current.anims.play('up', true);
+                    }
+                    else if (cursors.down.isDown) {
+                        player.current.setVelocityY(160);
+                        player.current.anims.play('down', true);
+                    }
+            
+                    if (
+                        !cursors.left.isDown && 
+                        !cursors.right.isDown && 
+                        !cursors.up.isDown && 
+                        !cursors.down.isDown
+                    ) {
+                        player.current.setVelocity(0);
+                        player.current.anims.pause();
+                    }
                 }
-            }
-
-
-            function hitBomb(player: any, bomb: any) {
-                this.physics.pause();
-
-                player.setTint(0xff0000);
-
-                player.anims.play('turn');
-
-                var gameOver = true;
-            }
-
-
-            function update() {
-                if (cursors.left.isDown) {
-                    player.setVelocityX(-160);
-
-                    player.anims.play('left', true);
-                }
-                else if (cursors.right.isDown) {
-                    player.setVelocityX(160);
-
-                    player.anims.play('right', true);
-                }
-                else {
-                    player.setVelocityX(0);
-
-                    player.anims.play('turn');
-                }
-
-                if (cursors.up.isDown && player.body?.touching.down) {
-                    player.setVelocityY(-330);
-                }
-
             }
         }
-    }, [])
+    }, []);
+
     return (
         <h1>Hello again</h1>
     )
